cf660263a7549ab16856cf01c279ad18b9d2d3b7
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-22 11:45:51 +0500

    updated code.

--- data/index.ts
+++
@@ -1,7 +0,0 @@
-export const gridItem = [
-    {title: "title1",description: "description1",id:1},
-    {title: "title1",description: "description1",id:1},
-    {title: "title1",description: "description1",id:1},
-    {title: "title1",description: "description1",id:1},
-    {title: "title1",description: "description1",id:1},
-]
