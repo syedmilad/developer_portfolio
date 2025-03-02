ce18d6684ab9ddf63dc1d60efa71f08ca92df946
syedmilad <syedmilad99@gmail.com>
2025-03-03 04:49:58 +0500

    updated code.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,25 @@
 export const projects = [
   {
+    title: "GPT-3",
+    description:
+      "This is a code repository for the gpt-3. using React & Custom CSS with BEM styling.",
+    image: "/images/gpt3.png",
+    tags: ["React", "Custom Css", "BEM"],
+    source: "https://gpt-3-delta-sandy.vercel.app/",
+    visit: "https://github.com/syedmilad/GPT3",
+    id: 3,
+  },
+  {
+    title: "Gerich Restaurant",
+    description:
+      "Using React & Tailwind I build a Bank UI UX - from start to finish.",
+    image: "/images/3.png",
+    tags: ["Tailwind", "React"],
+    source: "https://gerich-restaurant-blond-beta.vercel.app/",
+    visit: "https://github.com/syedmilad/Gerich-Restaurant",
+    id: 2,
+  },
+  {
     title: "Hero Bank",
     description:
       "Using React & Tailwind I build a Bank UI UX - from start to finish.",
@@ -19,30 +39,14 @@ export const projects = [
     visit: "https://github.com/syedmilad/Nexcent_UI-UX",
     id: 1,
   },
-  {
-    title: "Gerich Restaurant",
-    description:
-      "Using React & Tailwind I build a Bank UI UX - from start to finish.",
-    image: "/images/3.png",
-    tags: ["Tailwind", "React"],
-    source: "https://gerich-restaurant-blond-beta.vercel.app/",
-    visit: "https://github.com/syedmilad/Gerich-Restaurant",
-    id: 2,
-  },
-  {
-    title: 'GPT-3',
-    description: "This is a code repository for the gpt-3. using React & Custom CSS with BEM styling.",
-    image: '/images/gpt3.png',
-    tags: ['React', 'Custom Css', 'BEM'],
-    source: 'https://gpt-3-delta-sandy.vercel.app/',
-    visit: 'https://github.com/syedmilad/GPT3',
-    id: 3,
-  },
 ];
 
 export const TimeLineData = [
   { year: 2022, text: "Started my journey with udemy & freeCodeCamp" },
-  { year: 2023, text: "Intern Software Engineer (React Js & Core JavaScript), Grapner, INC" },
+  {
+    year: 2023,
+    text: "Intern Software Engineer (React Js & Core JavaScript), Grapner, INC",
+  },
   { year: 2024, text: "Software Engineer, Dimensional Sys, INC" },
   { year: 2025, text: "React Js Developer, Ornesol Sys, INC" },
 ];
